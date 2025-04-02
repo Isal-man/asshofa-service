d51fc4994ec02010332b726aecb0ecb9f7d5ac0b
Isal-man <faiskweren7@gmail.com>
2025-04-03 06:04:06 +0700

    add query when get users by usernameA

--- src/main/java/com/asshofa/management/repository/UsersRepository.java
+++ src/main/java/com/asshofa/management/repository/UsersRepository.java
@@ -2,7 +2,9 @@ package com.asshofa.management.repository;
 
 import com.asshofa.management.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UsersRepository extends JpaRepository<Users, Short> {
+    @Query("SELECT u FROM Users u WHERE u.username = :username")
     Users findByUsername(String username);
 }
